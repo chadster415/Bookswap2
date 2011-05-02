e1337905752f7b9636bcb2c160e44480d4085d15
Chad Armstrong <chad.armstrong@gmail.com>
2011-05-02 16:23:40 -0700

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookswap::Application.config.secret_token = 'e5b5b56befe957f584226268528652dfd2870e824452621fc494795daa46095f56a694eb8bb92b9c98341152efe94fec18198e36d066e773229beb83a394a7f7'
